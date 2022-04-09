d7da30a63a9eeb1339bad28f046e3116abaf86bd
Williams, Michael Owen <mwilliams606@gatech.edu>
2022-04-08 21:30:26 -0500

    Recursion attempt

--- MonsterDiagnosisAgent.py
+++ MonsterDiagnosisAgent.py
@@ -2,76 +2,77 @@ from itertools import combinations, product
 import numpy as np
 
 
-# convert patient vitamin values to integers
-# patient_vitamins = convert_symbols(patient)
-# convert disease vitamin values to integers
-# for disease in list(diseases.values()):
-#     convert_symbols(disease)
-# convert disease name values into their own array
-# names_list = []
-# for disease_name in list(diseases.keys()):
-#     names_list.append(disease_name)
+# # print dictionary
+# for k, v in diseases.items():
+#     print(k, '->', v)
 
-def convert_symbols(disease_dict):
-    for key, value in disease_dict.items():
-        if value == '0':
-            disease_dict[key] = 0
-        elif value == '+':
-            disease_dict[key] = 1
-        elif value == '-':
-            disease_dict[key] = -1
-
-    return disease_dict
-
-
-def combo_2(disease_combos):
+def generate_combinations(disease_combos, pair_count):
     combine_dict = {}
+    print("LENGTH", pair_count)
+    # print("Disease Combos")
+
+    # print(*list(disease_combos), sep="\n")
+    # for each combination in the set
     for item in disease_combos:
 
-        name_a = item[0][0]
-        name_b = item[1][0]
-        values_a = item[0][1]
-        # print(list(values_a.values()))
-        values_b = item[1][1]
-        # print(list(values_b.values()))
+        # access the first two sets of values
+        disease_a_name = item[0][0]
+        disease_b_name = item[1][0]
+        vitamins_a = item[0][1]
+        vitamins_b = item[1][1]
 
+        # sum the first two pairs
         sum_dict = {}
-        c_value = ''
-        for key, value in values_a.items():
+        combined_value = ''
+        # for each vitamin-level pair in disease_A
+        for vitamin, level in vitamins_a.items():
             # combination rules for positive vitamin
-            if value == '+':
-                if values_b.get(key) == '+':
-                    c_value = '+'
-                elif values_b.get(key) == '0':
-                    c_value = '+'
-                elif values_b.get(key) == '-':
-                    c_value = '0'
+            if level == '+':
+                if vitamins_b.get(vitamin) == '+':
+                    combined_value = '+'
+                elif vitamins_b.get(vitamin) == '0':
+                    combined_value = '+'
+                elif vitamins_b.get(vitamin) == '-':
+                    combined_value = '0'
             # combination rules for neutral vitamins
-            elif value == '0':
-                if values_b.get(key) == '+':
-                    c_value = '+'
-                elif values_b.get(key) == '0':
-                    c_value = '0'
-                elif values_b.get(key) == '-':
-                    c_value = '-'
+            elif level == '0':
+                if vitamins_b.get(vitamin) == '+':
+                    combined_value = '+'
+                elif vitamins_b.get(vitamin) == '0':
+                    combined_value = '0'
+                elif vitamins_b.get(vitamin) == '-':
+                    combined_value = '-'
             # combination rules for negative vitamins
-            elif value == '-':
-                if values_b.get(key) == '+':
-                    c_value = '0'
-                elif values_b.get(key) == '0':
-                    c_value = '0'
-                elif values_b.get(key) == '-':
-                    c_value = '-'
-            # build the sum dictionary for the combination
-            sum_dict[key] = c_value
-            # add the new dictionary as the value for the combination
-            combine_dict.update({name_a + "-" + name_b: sum_dict})
-    # print dictionary
-    for k, v in combine_dict.items():
-        print(k, '->', v)
+            elif level == '-':
+                if vitamins_b.get(vitamin) == '+':
+                    combined_value = '0'
+                elif vitamins_b.get(vitamin) == '0':
+                    combined_value = '0'
+                elif vitamins_b.get(vitamin) == '-':
+                    combined_value = '-'
 
-    return combine_dict
+            # store the summed dictionary values
+            sum_dict[vitamin] = combined_value
 
+            # add the sum dictionary as the value for the combination
+            combine_dict.update({disease_a_name + "-" + disease_b_name: sum_dict})
+
+
+    print("Combine Dict")
+    print(*list(disease_combos), sep="\n")
+
+    # if there were more than two pairs
+    if pair_count > 2:
+        # rebuild array
+
+        # call this function recursively
+        print("NEXT ROUND")
+        generate_combinations(list(combine_dict.items()), pair_count - 1)
+
+    # otherwise
+    else:
+        # return the results
+        return combine_dict
 
 
 def check_match(combine_dict, patient):
@@ -89,30 +90,36 @@ def check_match(combine_dict, patient):
 
 class MonsterDiagnosisAgent:
     def __init__(self):
-        # If you want to do any initial processing, add it here.
         pass
 
     def solve(self, diseases, patient):
-        diagnosis = None
 
-        # # print dictionary
-        # for k, v in diseases.items():
-        #     print(k, '->', v)
-        # # check with initial values
-        # diagnosis = check_match(diseases, patient)
-        #
-        # # check combinations of 2
-        # if diagnosis is None:
-        #     disease_combos = combinations(list(diseases.items()), 2)
-        #     combine_dict = combo_2(disease_combos)
-        #     diagnosis = check_match(combine_dict, patient)
+        # check with initial values
+        diagnosis = check_match(diseases, patient)
 
-        # check combinations of 3
-        if diagnosis is None:
-            disease_combos = combinations(list(diseases.items()), 3)
-            print(*list(disease_combos), sep='\n')
-            # combine_dict = combo_2(disease_combos)
-            # diagnosis = check_match(combine_dict, patient)
+        # increment the number of combinations
+        combo_counter = 2
 
+        # until a diagnosis is found
+        while diagnosis is None:
+
+            # arrange combinations
+            disease_combos = combinations(list(diseases.items()), combo_counter)
+            # print("DISEASE COMBOS")
+            # print(*list(disease_combos), sep="\n")
+
+            # perform combinations
+            combine_dict = generate_combinations(disease_combos, combo_counter)
+
+            # evaluate combinations
+            diagnosis = check_match(combine_dict, patient)
+
+            # move to next combo if the answer has not been found
+            combo_counter += 1
+            print("NEXT COMBO", combo_counter)
+
+            # temporary stop
+            if combo_counter == 4:
+                break
 
         return diagnosis
